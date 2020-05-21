5a38ef36e985d926da1f7d00c3b3cd5118acf025
rafael_gomes_santos <rafael.santos@concretesolutions.com.br>
2020-05-21 07:19:33 -0300

    desafio b2w - implementação metodo findById

--- desafio/src/main/java/com/b2w/desafio/PlanetaRepository.java
+++ desafio/src/main/java/com/b2w/desafio/PlanetaRepository.java
@@ -1,12 +1,16 @@
 package com.b2w.desafio;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "planeta", path = "planeta")
 public interface PlanetaRepository extends MongoRepository<Planeta, String> {
 	public List<Planeta> findByName(@Param("name") String name);
+	@Query("{ 'id' : ?0 }")
+	public Optional<Planeta> findById(@Param("id") String id);
 }
